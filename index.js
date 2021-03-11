3796a537dc0104c5650728739f96528e14271bd1
Guga <7043225+lgberro@users.noreply.github.com>
2021-03-11 12:58:05 -0300

    Done file input

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
+const fs = require("fs");
+const {max} = Math;
+
 const AIR = "A";
 const BRICK = "B";
 const WATER = "W";
 
-const input = [7, 10, 2, 5, 13, 3, 4, 1, 5, 9];
-const width = input.length;
-const height = Math.max(...input);
-const array = input.map((bricks, x) => [...Array(bricks).fill(BRICK), ...Array(height - bricks).fill(AIR)]);
-let total = 0;
+if (process.argv.length !== 3) return console.error("usage: node index.js {file path}");
+const path = process.argv[2];
 
-const get = (x, y) => array[x][y];
+fs.readFile(path, "utf8", (error, file) => {
+  if (error) return console.error(error);
+  // For code cleanliness there is NO input file validation (come on, that's easy to do :)
+  file
+    .split("\n") // splitting lines
+    .filter((string, line) => line && !(line % 2)) // getting only lines with silhouette
+    .map(string => string.split(" ").map(n => +n)) // isolating and converting chars to numbers
+    .forEach(input => {
+      const width = input.length;
+      const height = Math.max(...input);
+      const array = input.map((bricks, x) => [...Array(bricks).fill(BRICK), ...Array(height - bricks).fill(AIR)]);
+      let total = 0;
 
-const set = (x, y, value = get(x, y)) => (array[x][y] = value);
+      const get = (x, y) => array[x][y];
+      const set = (x, y, value = get(x, y)) => (array[x][y] = value);
+      const setSolid = (x, y) => set(x, y, get(x, y) === BRICK ? BRICK : WATER);
+      const setAir = (x, y) => set(x, y, get(x, y) === BRICK ? BRICK : AIR);
+      const isSolid = (x, y) => [BRICK, WATER].includes(get(max(x, 0), max(y, 0)));
+      const isAir = (x, y) => get(max(x, 0), max(y, 0)) === AIR;
 
-const setSolid = (x, y) => set(x, y, get(x, y) === BRICK ? BRICK : WATER);
+      const update = (x, y, left = get(x, y)) => {
+        const self = get(x, y);
+        if (x === width - 1) return; // rightmost can't be water
+        update(x + 1, y, self === BRICK ? BRICK : left); // recursing into right cell
+        // now every right cells in this row are defined
+        if (x === 0 || self === BRICK) return; // leftmost can't be water
+        const isLeftAir = left === AIR; // left cell isn't defined yet
+        const isLeftSolid = [BRICK, WATER].includes(left); // left cell isn't defined yet
+        const isDownAir = isAir(x, y - 1);
+        const isDownSolid = isSolid(x, y - 1);
+        const isRightAir = isAir(x + 1, y);
+        const isRightSolid = isSolid(x + 1, y);
+        if (isLeftAir || isDownAir || isRightAir) setAir(x, y);
+        if (isLeftSolid && isDownSolid && isRightSolid) ++total && setSolid(x, y);
+      };
 
-const setAir = (x, y) => set(x, y, get(x, y) === BRICK ? BRICK : AIR);
-
-const isSolid = (x, y) => [BRICK, WATER].includes(get(Math.max(x, 0), Math.max(y, 0)));
-
-const isAir = (x, y) => get(Math.max(x, 0), Math.max(y, 0)) === AIR;
-
-const update = (x, y, left = get(x, y)) => {
-  const self = get(x, y);
-
-  if (x === width - 1) return; // rightmost can't be water
-
-  update(x + 1, y, self === BRICK ? BRICK : left); // recursing into right cell
-
-  // now every right cells in this row are defined
-  const isLeftAir = left === AIR; // left cell isn't defined yet
-  const isLeftSolid = [BRICK, WATER].includes(left); // left cell isn't defined yet
-  const isDownAir = isAir(x, y - 1);
-  const isDownSolid = isSolid(x, y - 1);
-  const isRightAir = isAir(x + 1, y);
-  const isRightSolid = isSolid(x + 1, y);
-
-  if (x === 0 || self === BRICK) return; // leftmost can't be water
-  if (isLeftAir || isDownAir || isRightAir) setAir(x, y);
-  if (isLeftSolid && isDownSolid && isRightSolid) ++total && setSolid(x, y);
-};
-
-for (let y = 0; y < height; y++) update(0, y);
-
-array.forEach(x => console.log(...x));
-console.log(`Total flooded: ${total}`);
+      for (let y = 0; y < height; y++) update(0, y);
+      console.log(total);
+    });
+});
