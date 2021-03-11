2c6c40d3d68b22e9348a6002c6dce4bc1b8f4547
Guga <7043225+lgberro@users.noreply.github.com>
2021-03-11 12:15:17 -0300

    Fixed total miscounting

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ const isSolid = (x, y) => [BRICK, WATER].includes(get(Math.max(x, 0), Math.max(y
 
 const isAir = (x, y) => get(Math.max(x, 0), Math.max(y, 0)) === AIR;
 
-function update(x, y, left = get(x, y)) {
+const update = (x, y, left = get(x, y)) => {
   const self = get(x, y);
 
   if (x === width - 1) return; // rightmost can't be water
 
   update(x + 1, y, self === BRICK ? BRICK : left); // recursing into right cell
 
-  // now every right cells in the row are defined
+  // now every right cells in this row are defined
   const isLeftAir = left === AIR; // left cell isn't defined yet
   const isLeftSolid = [BRICK, WATER].includes(left); // left cell isn't defined yet
   const isDownAir = isAir(x, y - 1);
@@ -35,15 +35,12 @@ function update(x, y, left = get(x, y)) {
   const isRightAir = isAir(x + 1, y);
   const isRightSolid = isSolid(x + 1, y);
 
-  if (x === 0) return self; // leftmost can't be water
+  if (x === 0 || self === BRICK) return; // leftmost can't be water
   if (isLeftAir || isDownAir || isRightAir) setAir(x, y);
   if (isLeftSolid && isDownSolid && isRightSolid) ++total && setSolid(x, y);
-}
+};
 
-for (let y = 0; y < height; y++) {
-  console.log(total);
-  update(0, y);
-}
+for (let y = 0; y < height; y++) update(0, y);
 
 array.forEach(x => console.log(...x));
 console.log(`Total flooded: ${total}`);
